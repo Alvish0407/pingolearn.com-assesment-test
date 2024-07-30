84505b8a30c9e695c00f4cdd25f6a6e3dc2e32f9
Alvish Ramani <alvishramani.dev@gmail.com>
2024-07-30 17:12:13 +0530

    firebase configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pingo_learn_hiring_test/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
